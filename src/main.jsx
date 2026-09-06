a13b0db4e07f8ef713473e9a2d828659bd15c109
Rendyrta2405 <rendyrta2@gmail.com>
2026-09-06 16:23:07 +0700

    Add: posthog for web analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import posthog from 'posthog-js'
 import './styles.css'
+
+if (typeof window !== 'undefined') {
+  posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+    api_host: import.meta.env.VITE_POSTHOG_HOST,
+    person_profiles: 'identified_only'
+  })
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
